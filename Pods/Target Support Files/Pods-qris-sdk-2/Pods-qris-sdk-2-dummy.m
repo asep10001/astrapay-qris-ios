994334ce96a59e349ff812591e24448760bf3dad
Gilbert Subay <gilbertttsubay@gmail.com>
2022-03-16 10:55:30 +0700

    first push

---
+++ Pods/Target Support Files/Pods-qris-sdk-2/Pods-qris-sdk-2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qris_sdk_2 : NSObject
+@end
+@implementation PodsDummy_Pods_qris_sdk_2
+@end
---
+++ qris-sdk-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qris-sdk-2
+//
+//  Created by Gilbert on 3/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
